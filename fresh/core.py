3a344c1f182e7a40312cbf5e62f5e4ef5e0df61c
Ben <benjaminge.franklin@gmail.com>
2016-05-21 14:32:51 +0100

    Trying out some fresh stuff
    WARNING: Uses Python 3.x

---
+++ fresh/core.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+This project aims to provide basic AI functionality,
+along with any dumb ideas or features that we
+mindlessly add without need.
+
+Authors = Ben Rose, James 'Poo Head' Dibnah
+"""
+
+#imports
+import speech_recognition as sr
+import os
+
+def main():
+    print("this is working")
+
+
+main()
